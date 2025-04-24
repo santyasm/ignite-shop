0ed2b4c4bb099d931207259b800a673a6de6f46e
santyasm <yyasant5@gmail.com>
2025-04-24 17:56:16 -0300

    styles: added globalStyles

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,16 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Baloo+2:wght@400..800&family=Roboto+Mono:ital,wght@0,100..700;1,100..700&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
+          rel="stylesheet"
+        />
         <style
           id="stitches"
           dangerouslySetInnerHTML={{ __html: getCssText() }}
---
+++ src/styles/global.ts
@@ -0,0 +1,15 @@
+import { globalCss } from ".";
+
+export const globalStyles = globalCss({
+  "*": {
+    margin: 0,
+    padding: 0,
+  },
+  body: {
+    "-webkit-font-smoothing": "antialiased",
+  },
+  "body, input, textarea, button": {
+    fontFamily: "Roboto",
+    fontWeight: 400,
+  },
+});
